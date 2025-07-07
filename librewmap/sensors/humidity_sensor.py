74ddc1c3096c97dec66ddf40693ff8997b6bfeee
vajra77 <f.ferreri@namex.it>
2025-07-07 12:49:08 +0200

    added update methods

--- librewmap/sensors/humidity_sensor.py
+++ librewmap/sensors/humidity_sensor.py
@@ -1,3 +1,4 @@
+import requests
 from .sensor import Sensor
 
 
@@ -81,3 +82,30 @@ class HumiditySensor(Sensor):
 
         return result
 
+
+    def update(self, api_url, api_key):
+        url = f"{api_url}/devices/{self.device_id}/health/temperature/{self.id}"
+        response = requests.get(url=url, headers={"X-Auth-Token": api_key})
+        data = response.json()
+
+        t_prev = data['graphs'][0]['sensor_prev']
+        t_cur = data['graphs'][0]['sensor_current']
+        t_warn = data['graphs'][0]['sensor_limit_warn']
+        t_crit = data['graphs'][0]['sensor_limit']
+
+        if t_prev > t_cur:
+            self.trend = -1
+        elif t_prev < t_cur:
+            self.trend = 1
+        else:
+            self.trend = 0
+
+        if t_warn is not None:
+            if t_cur < t_warn:
+                self.alarm = 'ok'
+            elif t_warn <= t_cur < t_crit:
+                self.alarm = 'warn'
+            else:
+                self.alarm = 'crit'
+
+        self.last = t_cur
--- librewmap/sensors/temperature_sensor.py
+++ librewmap/sensors/temperature_sensor.py
@@ -8,32 +8,6 @@ class TemperatureSensor(Sensor):
     def type(self) -> str:
         return "temperature"
 
-    def update(self, api_url, api_key):
-        url = f"{api_url}/devices/{self.device_id}/health/temperature/{self.id}"
-        response = requests.get(url=url, headers={"X-Auth-Token": api_key})
-        data = response.json()
-
-        t_prev = data['graphs'][0]['sensor_prev']
-        t_cur = data['graphs'][0]['sensor_current']
-        t_warn = data['graphs'][0]['sensor_limit_warn']
-        t_crit = data['graphs'][0]['sensor_limit']
-
-        if t_prev > t_cur:
-            self.trend = -1
-        elif t_prev < t_cur:
-            self.trend = 1
-        else:
-            self.trend = 0
-
-        if t_warn is not None:
-            if t_cur < t_warn:
-                self.alarm = 'ok'
-            elif t_warn <= t_cur < t_crit:
-                self.alarm = 'warn'
-            else:
-                self.alarm = 'crit'
-
-        self.last = t_cur
 
     @property
     def css(self) -> str:
@@ -110,3 +84,31 @@ class TemperatureSensor(Sensor):
             result = f"<p><i class='fa-solid fa-temperature-half'></i>&nbsp;&nbsp;{self.last}</p>\n"
 
         return result
+
+
+    def update(self, api_url, api_key):
+        url = f"{api_url}/devices/{self.device_id}/health/temperature/{self.id}"
+        response = requests.get(url=url, headers={"X-Auth-Token": api_key})
+        data = response.json()
+
+        t_prev = data['graphs'][0]['sensor_prev']
+        t_cur = data['graphs'][0]['sensor_current']
+        t_warn = data['graphs'][0]['sensor_limit_warn']
+        t_crit = data['graphs'][0]['sensor_limit']
+
+        if t_prev > t_cur:
+            self.trend = -1
+        elif t_prev < t_cur:
+            self.trend = 1
+        else:
+            self.trend = 0
+
+        if t_warn is not None:
+            if t_cur < t_warn:
+                self.alarm = 'ok'
+            elif t_warn <= t_cur < t_crit:
+                self.alarm = 'warn'
+            else:
+                self.alarm = 'crit'
+
+        self.last = t_cur
