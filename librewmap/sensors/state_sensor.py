174939ae25aaa2a8fe8c8779b39240f60050b47f
vajra77 <f.ferreri@namex.it>
2025-07-18 11:59:02 +0200

    trying SVG backgrounds

--- librewmap/sensors/state_sensor.py
+++ librewmap/sensors/state_sensor.py
@@ -12,11 +12,11 @@ class StateSensor(Sensor):
     def html(self) -> str:
         match self.alarm:
             case "ok":
-                return f"<p><img class='svg-ok' src='images/ok.svg'>{self.name}</p>\n"
+                return f"<p><img src='images/ok.svg'>{self.name}</p>\n"
             case "warn":
-                return f"<p><img class='svg-warn' src='images/bell_alarm.svg'>{self.name}</p>\n"
+                return f"<p><img src='images/bell_alarm.svg'>{self.name}</p>\n"
             case "crit":
-                return f"<p><img class='svg-crit' src='images/bell_alarm.svg'>{self.name}</p>\n"
+                return f"<p><img src='images/bell_alarm.svg'>{self.name}</p>\n"
         return ""
 
     @property
@@ -29,31 +29,33 @@ class StateSensor(Sensor):
             left: {self.left}px;
         """
 
-        if self.alarm == 'ok':
-            css += """
-            width: 80px;
-            height: 80px;
-            animation: blink 3s linear infinite;
-            /* background: radial-gradient(rgb(52,73,94,1.0), rgb(255,255,255,0.2)); */
-            """
-        elif self.alarm == 'warn':
-            css += """
-            height: 100px;
-            width: 100px;
-            animation: blink 1.5s linear infinite; 
-            /* background: radial-gradient(rgb(211,84,0,1.0), rgb(255,255,255,0.2)); */
-            """
-        else:
-            css += """
-            height: 100px;
-            width: 100px;
-            animation: blink 0.5s linear infinite; 
-            /* background: radial-gradient(rgb(176,58,46,1.0), rgb(255,255,255,0.2)); */
-            """
+        match self.alarm:
+            case "ok":
+                css += """
+                    width: 80px;
+                    height: 80px;
+                    animation: blink 3s linear infinite;
+                    /* background: radial-gradient(rgb(52,73,94,1.0), rgb(255,255,255,0.2)); */
+                }
+                """
+            case 'warn':
+                css += """
+                    width: 100px;
+                    height: 100px;
+                    animation: blink 1.5s linear infinite; 
+                    /* background: radial-gradient(rgb(211,84,0,1.0), rgb(255,255,255,0.2)); */
+                }
+                """
+            case 'crit':
+                css += """
+                    width: 100px;
+                    height: 100px;
+                    animation: blink 0.5s linear infinite; 
+                    /* background: radial-gradient(rgb(176,58,46,1.0), rgb(255,255,255,0.2)); */
+                }
+                """
 
         css += f"""
-        }}
-        
         .{self.name}:hover {{
             animation: none;
         }}
@@ -65,23 +67,50 @@ class StateSensor(Sensor):
             font-weight: bold;
         """
 
-        if self.alarm == 'ok':
-            css += """
-            height: 40px;
-            color: green;
-            """
-        elif self.alarm == 'warn':
-            css += """
-            height: 60px
-            color: orange;
-            """
-        else:
-            css += """
-            height: 80px
-            color: red;
-            """
+        match self.alarm:
+            case "ok":
+                css += """
+                    height: 40px;
+                    color: green;
+                }
+                """
+            case "warn":
+                css += """
+                    height: 60px
+                    color: orange;
+                }
+                """
+            case "crit":
+                css += """
+                    height: 80px
+                    color: red;
+                }
+                """
 
-        css += "}"
+        css += f""" 
+        .{self.name} img.svg {{
+            padding-right: 5px;
+        """
+
+        match self.alarm:
+            case "ok":
+                css += """
+                    width: 20px;
+                    fill: green;
+                }
+                """
+            case "warn":
+                css += """
+                    width: 30px;
+                    fill: orange;
+                }
+                """
+            case "crit":
+                css += """
+                    width: 40px;
+                    fill: red;
+                }
+                """
 
         return css
 
