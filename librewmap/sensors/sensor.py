525d5a83bddd143158549f7f54f8226ad59bd87f
vajra77 <f.ferreri@namex.it>
2025-07-23 09:36:25 +0200

    changing blink

--- librewmap/sensors/sensor.py
+++ librewmap/sensors/sensor.py
@@ -65,7 +65,7 @@ class Sensor:
     def blink(self) -> float:
         match self._alarm:
             case "ok":
-                return 3
+                return 5
             case "warn":
                 return 1.5
             case "crit":
