7fa480d4b7babd35bed729e4248937ebf2939b6b
vajra77 <f.ferreri@namex.it>
2025-07-23 07:58:42 +0200

    added Power

--- librewmap/sensors/power_sensor.py
+++ librewmap/sensors/power_sensor.py
@@ -34,8 +34,8 @@ class PowerSensor(Sensor):
 
         t_prev = data['graphs'][0]['sensor_prev']
         t_cur = data['graphs'][0]['sensor_current']
-        t_warn = data['graphs'][0]['sensor_limit_warn'] or 1000
-        t_crit = data['graphs'][0]['sensor_limit'] or 1000
+        t_warn = data['graphs'][0]['sensor_limit_warn'] or 1000000
+        t_crit = data['graphs'][0]['sensor_limit'] or 1000000
 
         if t_prev > t_cur:
             self._trend = -1
