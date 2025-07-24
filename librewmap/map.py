46f76e45bdd2a8e71452baee88def7e5512a53d2
vajra77 <f.ferreri@namex.it>
2025-07-24 09:51:26 +0200

    removed dependency from Dataclass

--- librewmap/map.py
+++ librewmap/map.py
@@ -1,30 +1,59 @@
 import yaml
-from dataclasses import dataclass
-from typing import List
-from librewmap.sensors import (Sensor, TemperatureSensor,
-                               HumiditySensor, StateSensor, InvertedStateSensor,
-                               LoadSensor, PowerSensor)
+from librewmap.sensors import Sensor, TemperatureSensor, HumiditySensor, StateSensor, InvertedStateSensor, \
+                               LoadSensor, PowerSensor
 
 
-@dataclass
 class Map:
-    name: str
-    title: str
-    image: str
-    css: str
-    next: str
-    api_url: str
-    api_key: str
-    nms_url: str
-    sensors: List[Sensor]
+
+    def __init__(self, name, title, image, css, api_url, api_key, nms_url):
+        self._name = name
+        self._title = title
+        self._image = image
+        self._css = css
+        self._api_url = api_url
+        self._api_key = api_key
+        self._nms_url = nms_url
+        self._sensors = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def image(self):
+        return self._image
+
+    @property
+    def css(self):
+        return self._css
+
+    @property
+    def api_url(self):
+        return self._api_url
+
+    @property
+    def api_key(self):
+        return self._api_key
+
+    @property
+    def nms_url(self):
+        return self._nms_url
+
+    @property
+    def sensors(self):
+        return self._sensors
 
     def register_sensor(self, sensor: Sensor):
-        self.sensors.append(sensor)
+        self._sensors.append(sensor)
 
     def retrieve_data(self):
-        for sensor in self.sensors:
+        for sensor in self._sensors:
             try:
-                sensor.update(self.api_url, self.api_key)
+                sensor.update(self._api_url, self._api_key)
             except Exception as e:
                 print(f"[ERR]: sensor {sensor.name} = {e}")
                 continue
@@ -32,7 +61,7 @@ class Map:
     @property
     def alarm(self):
         result = False
-        for sensor in self.sensors:
+        for sensor in self._sensors:
             if sensor.alarm != "ok":
                 result = True
                 break
@@ -48,11 +77,9 @@ class Map:
             title=conf['map']['title'],
             image=conf['map']['image'],
             css=conf['map']['css'],
-            next=conf['map']['next'],
             api_url=conf['map']['api_url'],
             api_key=conf['map']['api_key'],
             nms_url=conf['map']['nms_url'],
-            sensors=[]
         )
 
         for sconf in conf['sensors']:
@@ -74,6 +101,4 @@ class Map:
             this_map.register_sensor(sensor)
 
         return this_map
-
-
 
